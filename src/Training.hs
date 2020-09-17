f33847ea60944a6be4aa3a7dfacc5ad0b7a27e53
Stefan <Stefan.Steg@gmail.com>
2020-09-17 22:27:24 +0200

    added training functionality

---
+++ src/Training.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Training
+  ( TrainParams (..)
+  , train
+  , gradientDescent
+  ) where
+
+import Control.Lens
+import Data.List (transpose)
+import Data.List.Split (chunksOf)
+
+import Linalg hiding (transpose)
+import Network
+
+data TrainParams = TrainParams { _inps :: Matrix
+                               , _lbls :: Matrix
+                               , _epochs :: Int 
+                               , _batchSize :: Int
+                               , _opt :: Matrix -> Matrix -> Matrix }
+makeLenses ''TrainParams
+
+gradientDescent :: Double -> Matrix -> Matrix -> Matrix
+gradientDescent alpha grad mat = mat `sub` (alpha `smul` grad)
+
+-- | TODO: Write description
+train :: TrainParams -> Network -> IO Network
+train params net = train' params net $ params ^. epochs  
+train' params net 0 = return net
+train' params net e = do
+  let nextNet = trainEpoch params net
+  putStrLn $ "Epoch " ++ show ((params ^. epochs) - e + 1) ++ ":"
+  train' params nextNet $ e - 1
+
+
+trainEpoch :: TrainParams -> Network -> Network
+trainEpoch params net = foldl (trainBatch params) net batches
+  where batches = chunksOf (params ^. batchSize) $ zip (params ^. inps) (params ^. lbls)
+
+
+-- | Returns network with updated weights, biases and no input layer
+trainBatch :: TrainParams -> Network -> [([Double],[Double])] -> Network 
+trainBatch params net batch = net & layers .~ updatedDenseLyrs 
+  where gradients = processBatch net batch  
+        updateWeights d g = Dense (d & weights %~ (params ^. opt) (g ^. dw)
+                                        & bias %~ (params ^. opt) (g ^. db))
+        updatedDenseLyrs = zipWith updateWeights ([d | Dense d <- net ^. layers]) gradients
+
+
+processBatch :: Network -> [([Double],[Double])] -> [Gradient] 
+processBatch net batch = map normalize sumGrads
+  where normalize g = g & dw %~ smul (1 / fromIntegral (length batch)) 
+                        & db %~ smul (1 / fromIntegral (length batch))
+        newInputNet inp n = n & layers .~ (Input inp : [Dense d | Dense d <- n ^. layers])
+        step (x, t) = backprop (feedForward $ newInputNet [x] net) [t] 
+        gradients = map step batch
+        modGrad sum gr = sum & dw %~ add (gr ^. dw)
+                             & db %~ add (gr ^. db)
+        dummyGrad = Gradient { _dw = zeros 1 1, _db = zeros 1 1 }
+        sumGrads = map (foldl modGrad dummyGrad) $ transpose gradients 
