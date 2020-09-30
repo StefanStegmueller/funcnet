6b7ad4f3b31b9f943dab4ba27f56cb9b275652e4
Stefan <Stefan.Steg@gmail.com>
2020-09-30 17:26:28 +0200

    added MNIST example

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,9 @@
 module Main where
 
 import Prelude hiding (tanh)
+import System.IO
 import Control.Lens
+import Data.List.Split
 
 import Network 
 import Linalg
@@ -10,36 +12,53 @@ import Activation
 import Loss
 import Training
 
--- Data x and labels t
-x = [[1, 1]
-    ,[0, 1]
-    ,[1, 0]
-    ,[0, 0]]
-
-t = [[0]
-    ,[1]
-    ,[1]
-    ,[0]]
+dataPath = "./data/"
+trainFileX = dataPath ++ "mnist_small_train_in.txt"
+trainFileT = dataPath ++ "mnist_small_train_out.txt"
+testFileX = dataPath ++ "mnist_small_test_in.txt"
+testFileT = dataPath ++ "mnist_small_test_out.txt"
 
 main :: IO ()
 main = do
+
+  -- Load data
+  xTrain <- readInputData trainFileX 
+  tTrain <- readLabelData trainFileT
+  xTest <- readInputData testFileX
+  tTest <- readLabelData testFileT
+
+  -- Initialize network and parameters 
   let net = initNetwork
-  let p = hParams 
+  let p = hParams xTrain tTrain
+
+  -- Train network
   train p net
   return ()
 
-initNetwork :: Network 
-initNetwork = Network layers squaredError  
-  where init = (he 1234)
-        layers = compose [ (Input x      , 2, init)
-                         , (dense relu   , 2, init)
-                         , (dense tanh   , 1, init)
-                         , (dense sigmoid, 3, init)
-                         , (dense sigmoid, 1, init)]
+readInputData :: String -> IO Matrix
+readInputData fpath = do
+  contents <- readFile fpath 
+  let strings = map (splitOn ",") $ lines contents
+  let mat = (map . map) (\s -> read s :: Double) strings
+  return mat 
 
-hParams :: TrainParams
-hParams = TrainParams { _inps = x
-                      , _lbls = t
-                      , _epochs = 3
-                      , _batchSize = 2
-                      , _opt = (gradientDescent 0.03)}
+readLabelData :: String -> IO Matrix
+readLabelData fpath = do
+  contents <- readFile fpath
+  let labels = map (\s -> read s :: Integer) $ lines contents
+  let onehot n = [if i == n then 1 else 0 | i <- [0..9]]
+  return (map onehot labels)
+
+initNetwork :: Network 
+initNetwork = Network layers binaryCrossEntropy   
+  where init = (he 1234)
+        layers = compose [ (Input [[0]]  , 784, init)
+                         , (dense relu   , 100, init)
+                         , (dense softmax, 10, init)]
+
+hParams :: Matrix -> Matrix -> TrainParams
+hParams x t = TrainParams { _inps = x
+                          , _lbls = t
+                          , _epochs = 3
+                          , _batchSize = 10
+                          , _opt = (gradientDescent 0.03)}
