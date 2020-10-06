45bebb9e59403a187ec7d999d6a911008d5f2c98
Stefan <Stefan.Steg@gmail.com>
2020-10-06 18:31:27 +0200

    fixed linalg tests to use vectors

--- test/LinalgSpec.hs
+++ test/LinalgSpec.hs
@@ -1,65 +1,99 @@
 module LinalgSpec (spec) where
 
 import Control.Exception (evaluate)
+import qualified Data.Vector as V
 import Linalg
 import Test.Hspec
+import Util (fromList2)
+
+-- test transpose matrices
+m1 = fromList2 [[1, 2], [3, 4]]
+
+m2 = fromList2 [[1, 3], [2, 4]]
+
+m3 = fromList2 [[1, 2], [1, 2, 3]]
+
+-- test add, sub, hadamard matrices
+m4 = fromList2 [[1, 2], [2, 1]]
+
+m5 = fromList2 [[2, 1], [1, 2]]
+
+m6 = fromList2 [[3, 3], [3, 3]]
+
+m7 = fromList2 [[1, 2], [1]]
+
+m8 = fromList2 [[1], [1]]
+
+m9 = fromList2 [[-1, 1], [1, -1]]
+
+m16 = fromList2 [[2, 2], [2, 2]]
+
+-- test matmul matrices
+m10 = fromList2 [[1, 2], [2, 1], [1, 2]]
+
+m11 = fromList2 [[2, 1, 2], [1, 2, 1]]
+
+m12 = fromList2 [[4, 5, 4], [5, 4, 5], [4, 5, 4]]
+
+m13 = fromList2 [[1], [1, 2]]
+
+-- test smul matrices
+m14 = fromList2 [[1, 2], [3, 4]]
+
+m15 = fromList2 [[2, 4], [6, 8]]
 
 spec :: Spec
 spec = do
   describe "transpose" $ do
     it "returns the correct transposed matrix" $
-      transpose [[1, 2], [3, 4]] `shouldBe` [[1, 3], [2, 4]]
+      transpose m1 `shouldBe` m2
 
     it "throws an exception because of inconsistent matrix" $
-      evaluate (transpose [[1, 2], [1, 2, 3]]) `shouldThrow` anyErrorCall
+      evaluate (transpose m3) `shouldThrow` anyErrorCall
 
   describe "add" $ do
     it "returns the correct sum" $
-      add [[1, 2], [2, 1]] [[2, 1], [1, 2]] `shouldBe` [[3, 3], [3, 3]]
+      add m4 m5 `shouldBe` m6
 
     it "throws an exception because of inconsistent matrix" $
-      evaluate (add [[1, 2], [1]] [[1], [1]]) `shouldThrow` anyErrorCall
+      evaluate (add m7 m8) `shouldThrow` anyErrorCall
 
     it "throws an exception because of different matrix dimensions" $
-      evaluate (add [[1, 2], [1, 2]] [[1], [1]]) `shouldThrow` anyErrorCall
+      evaluate (add m4 m8) `shouldThrow` anyErrorCall
 
   describe "sub" $ do
     it "returns the correct subtraction" $
-      sub [[1, 2], [2, 1]] [[2, 1], [1, 2]] `shouldBe` [[-1, 1], [1, -1]]
+      sub m4 m5 `shouldBe` m9
 
     it "throws an exception because of inconsistent matrix" $
-      evaluate (sub [[1, 2], [1]] [[1], [1]]) `shouldThrow` anyErrorCall
+      evaluate (sub m7 m8) `shouldThrow` anyErrorCall
 
     it "throws an exception because of different matrix dimensions" $
-      evaluate (sub [[1, 2], [1, 2]] [[1], [1]]) `shouldThrow` anyErrorCall
+      evaluate (sub m4 m8) `shouldThrow` anyErrorCall
 
   describe "matmul" $ do
     it "returns the correct product" $
-      matmul [[1, 2], [2, 1], [1, 2]] [[2, 1, 2], [1, 2, 1]] `shouldBe` [[4, 5, 4], [5, 4, 5], [4, 5, 4]]
+      matmul m10 m11 `shouldBe` m12
 
     it "throws an exception because of inconsistent matrix" $
-      evaluate (matmul [[1, 2], [1]] [[1], [1, 2]]) `shouldThrow` anyErrorCall
+      evaluate (matmul m7 m13) `shouldThrow` anyErrorCall
 
     it "throws an exception because matrix dimensions not matching" $ do
-      let mat =
-            [ [1, 2, 3],
-              [1, 2, 3]
-            ]
-      evaluate (mat `matmul` mat) `shouldThrow` anyErrorCall
+      evaluate (m10 `matmul` m10) `shouldThrow` anyErrorCall
 
   describe "smul" $ do
     it "returns the correct product" $
-      smul 2 [[1, 2], [3, 4]] `shouldBe` [[2, 4], [6, 8]]
+      smul 2 m14 `shouldBe` m15
 
     it "throws an exception because of inconsistent matrix" $
-      evaluate (smul 2 [[1, 2], [1]]) `shouldThrow` anyErrorCall
+      evaluate (smul 2 m7) `shouldThrow` anyErrorCall
 
   describe "hadamard" $ do
     it "returns the correct hadamard product" $
-      hadamard [[1, 2], [2, 1]] [[2, 1], [1, 2]] `shouldBe` [[2, 2], [2, 2]]
+      hadamard m4 m5 `shouldBe` m16
 
     it "throws an exception because of inconsistent matrix" $
-      evaluate (hadamard [[1, 2], [1]] [[1], [1]]) `shouldThrow` anyErrorCall
+      evaluate (hadamard m7 m8) `shouldThrow` anyErrorCall
 
     it "throws an exception because of different matrix dimensions" $
-      evaluate (hadamard [[1, 2], [1, 2]] [[1], [1]]) `shouldThrow` anyErrorCall
+      evaluate (hadamard m4 m8) `shouldThrow` anyErrorCall
